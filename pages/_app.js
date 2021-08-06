e22103cfa2225b62ade36bbfefbe6aaf0b4fcb0b
victordantasdev <joaovictordjardim@gmail.com>
2021-08-06 18:04:48 -0300

    :art: eslint e prop-types adicionados e configurados

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -6,21 +8,27 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-`
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
+
+App.propTypes = {
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.func.isRequired,
+};
